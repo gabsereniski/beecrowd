e710940aac5a12a3203ea363f42e37af1daa787e
gabsereniski <gabsereniski@gmail.com>
2022-12-28 14:25:53 -0300

    update 1234

--- solutions/1234.cpp
+++ solutions/1234.cpp
@@ -1,43 +1,29 @@
-#include <stdio.h>
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main()
+{
+      string s;
 
-    int n, danca;
-    char texto[50];
-    
-    while(std::cin.getline(texto,50)){
-        danca = 0;
-        for(int j = 0; j < 50; j++){
-            if(texto[j] == '\0') break;
-            if(danca == 0){
-                     if(texto[j] >= 97){
-                                  printf("%c",texto[j]-32);
-                                  danca = 1;
-                     }
-                     else if(texto[j] == 32){
-                           printf("%c",texto[j]);
-                     }
-                     else{
-                           printf("%c",texto[j]);
-                           danca = 1;
-                     }
-            }else{
-                  if(texto[j] >= 97){
-                               printf("%c",texto[j]);
-                               danca = 0;
-                  }
-                  else if(texto[j] == 32){
-                        printf("%c",texto[j]);
-                  }
-                  else{
-                        printf("%c",texto[j] + 32);
-                        danca = 0;
+      while(getline(cin, s))
+      {
+            int last = 0;
+            for(int i = 0; i < s.size(); i++)
+            {
+                  if(isalpha(s[i]))
+                  {
+                        if(last == 0)
+                        {
+                              s[i] = toupper(s[i]);
+                              last = 1;
+                        }
+                        else
+                        {
+                              s[i] = tolower(s[i]);
+                              last = 0;
+                        }
                   }
             }
-         }
-         printf("\n");
-    }
-    
-    return 0;
+            cout << s << endl;
+      }
 }
